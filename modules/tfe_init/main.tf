936f471db3acc2d098e6537118a4b0ad7aff4340
anniehedgpeth <annie@hashicorp.com>
2022-01-10 14:01:28 -0700

    formatting

--- modules/tfe_init/main.tf
+++ modules/tfe_init/main.tf
@@ -12,11 +12,11 @@ locals {
       tls_bootstrap_key_pathname  = var.tls_bootstrap_key_pathname
 
       # Secrets
-      ca_certificate_secret = var.ca_certificate_secret
-      certificate_secret    = var.certificate_secret
-      key_secret            = var.key_secret
-      tfe_license_file_location  = var.tfe_license_file_location
-      tfe_license_secret    = var.tfe_license_secret
+      ca_certificate_secret     = var.ca_certificate_secret
+      certificate_secret        = var.certificate_secret
+      key_secret                = var.key_secret
+      tfe_license_file_location = var.tfe_license_file_location
+      tfe_license_secret        = var.tfe_license_secret
 
       # Proxy information
       proxy_ip   = var.proxy_ip
