5c7b6cc7bcb8c53a5aab20d916f0f978938e5396
anniehedgpeth <annie@hashicorp.com>
2022-01-18 12:55:06 -0700

    jsonencode output

--- modules/settings/outputs.tf
+++ modules/settings/outputs.tf
@@ -1,7 +1,7 @@
 output "config" {
   value = {
-    replicated_configuration = local.replicated_configuration
-    tfe_configuration        = local.tfe_configuration
+    replicated_configuration = jsonencode(local.replicated_configuration)
+    tfe_configuration        = jsonencode(local.tfe_configuration)
   }
 
   description = "The settings that will be used to configure Replicated and Terraform Enterprise."
