7ae0584ea79896100066b92fd28ee7a311307cfd
John Kerry <john.kerry@hashicorp.com>
2022-04-15 12:13:04 -0400

    Gcp init utilities (#16)
    * adding scaffolding for the monitoring agent installation function and stubbing google existing functions.
    
    * removing unintentional terraform interpolation code
    
    * add monitoring function
    
    * no proxy for GCP secret cmd
    
    * add GCP path for yum repo
    
    * moving gpg patch up to top to avoid race condition
    
    * remove yum makecache
    
    * add mounted disk
    
    * fix syntax error - curly bracket
    
    * pg_optional_configs only if a/a
    
    * external validation
    
    * removing setenforce and firewalld management commands from google rhel instances
    
    * add gcp docker config and tbw pull
    
    * docker daemon
    
    * add docker_config var and alphabetize
    
    * remove empty space
    
    Co-authored-by: anniehedgpeth <annie@hashicorp.com>

--- modules/tfe_init/main.tf
+++ modules/tfe_init/main.tf
@@ -5,19 +5,25 @@ locals {
     "${path.module}/templates/tfe.sh.tpl",
     {
       # Functions
-      get_base64_secrets = data.template_file.get_base64_secrets.rendered
-      install_packages   = data.template_file.install_packages.rendered
+      get_base64_secrets        = data.template_file.get_base64_secrets.rendered
+      install_packages          = data.template_file.install_packages.rendered
+      install_monitoring_agents = data.template_file.install_monitoring_agents.rendered
 
       # Configuration data
-      cloud                       = var.cloud
-      distribution                = var.distribution
       active_active               = var.tfe_configuration.enable_active_active.value == "1" ? true : false
+      airgap_url                  = var.airgap_url
+      airgap_pathname             = try(var.replicated_configuration.LicenseBootstrapAirgapPackagePath, null)
+      cloud                       = var.cloud
+      custom_image_tag            = try(var.tfe_configuration.custom_image_tag.value, null)
+      disk_path                   = var.disk_path
+      disk_device_name            = var.disk_device_name
+      distribution                = var.distribution
+      docker_config               = filebase64("${path.module}/files/daemon.json")
+      enable_monitoring           = var.enable_monitoring != null ? var.enable_monitoring : false
       replicated                  = base64encode(jsonencode(var.replicated_configuration))
       settings                    = base64encode(jsonencode(var.tfe_configuration))
       tls_bootstrap_cert_pathname = try(var.replicated_configuration.TlsBootstrapCert, null)
       tls_bootstrap_key_pathname  = try(var.replicated_configuration.TlsBootstrapKey, null)
-      airgap_url                  = var.airgap_url
-      airgap_pathname             = try(var.replicated_configuration.LicenseBootstrapAirgapPackagePath, null)
 
       # Secrets
       ca_certificate_secret_id  = var.ca_certificate_secret_id
@@ -44,6 +50,15 @@ data "template_file" "get_base64_secrets" {
 
 data "template_file" "install_packages" {
   template = file("${path.module}/templates/install_packages.func")
+
+  vars = {
+    cloud        = var.cloud
+    distribution = var.distribution
+  }
+}
+
+data "template_file" "install_monitoring_agents" {
+  template = file("${path.module}/templates/install_monitoring_agents.func")
 
   vars = {
     cloud        = var.cloud
