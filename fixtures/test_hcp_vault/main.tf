f52b95700c80905b0befe7a310a2142bfe2e290e
anniehedgpeth <annie@hashicorp.com>
2022-05-18 11:03:56 -0600

    test broken vault module

--- fixtures/test_hcp_vault/main.tf
+++ fixtures/test_hcp_vault/main.tf
@@ -1,77 +1,77 @@
-# Vault AppRole
-# -------------
-resource "hcp_vault_cluster" "test" {
-  cluster_id      = var.hcp_vault_cluster_id
-  hvn_id          = var.hcp_vault_cluster_hvn_id
-  public_endpoint = var.hcp_vault_cluster_public_endpoint
-  tier            = var.hcp_vault_cluster_tier
-}
+# # Vault AppRole
+# # -------------
+# resource "hcp_vault_cluster" "test" {
+#   cluster_id      = var.hcp_vault_cluster_id
+#   hvn_id          = var.hcp_vault_cluster_hvn_id
+#   public_endpoint = var.hcp_vault_cluster_public_endpoint
+#   tier            = var.hcp_vault_cluster_tier
+# }
 
-resource "hcp_vault_cluster_admin_token" "test" {
-  cluster_id = hcp_vault_cluster.test.cluster_id
-}
+# resource "hcp_vault_cluster_admin_token" "test" {
+#   cluster_id = hcp_vault_cluster.test.cluster_id
+# }
 
-# Vault Policy
-# -------------
-resource "vault_policy" "ptfe" {
-  name = var.vault_policy_name
+# # Vault Policy
+# # -------------
+# resource "vault_policy" "ptfe" {
+#   name = var.vault_policy_name
 
-  policy = <<EOT
-    path "auth/approle/login" {
-      capabilities = ["create", "read"]
-    }
-    path "sys/renew/*" {
-      policy = "write"
-    }
-    path "auth/token/renew/*" {
-      policy = "write"
-    }
-    path "transit/encrypt/atlas_*" {
-      capabilities = ["create", "update"]
-    }
-    path "transit/decrypt/atlas_*" {
-      capabilities = ["update"]
-    }
-    path "transit/encrypt/archivist_*" {
-      capabilities = ["create", "update"]
-    }
-    # For decrypting datakey ciphertexts.
-    path "transit/decrypt/archivist_*" {
-      capabilities = ["update"]
-    }
-    # To upsert the transit key used for datakey generation.
-    path "transit/keys/archivist_*" {
-      capabilities = ["create", "update"]
-    }
-    # For performing key derivation.
-    path "transit/datakey/plaintext/archivist_*" {
-      capabilities = ["update"]
-    }
-    # For health checks to read the mount table.
-    path "sys/mounts" {
-      capabilities = ["read"]
-    }
-EOT
-}
+#   policy = <<EOT
+#     path "auth/approle/login" {
+#       capabilities = ["create", "read"]
+#     }
+#     path "sys/renew/*" {
+#       policy = "write"
+#     }
+#     path "auth/token/renew/*" {
+#       policy = "write"
+#     }
+#     path "transit/encrypt/atlas_*" {
+#       capabilities = ["create", "update"]
+#     }
+#     path "transit/decrypt/atlas_*" {
+#       capabilities = ["update"]
+#     }
+#     path "transit/encrypt/archivist_*" {
+#       capabilities = ["create", "update"]
+#     }
+#     # For decrypting datakey ciphertexts.
+#     path "transit/decrypt/archivist_*" {
+#       capabilities = ["update"]
+#     }
+#     # To upsert the transit key used for datakey generation.
+#     path "transit/keys/archivist_*" {
+#       capabilities = ["create", "update"]
+#     }
+#     # For performing key derivation.
+#     path "transit/datakey/plaintext/archivist_*" {
+#       capabilities = ["update"]
+#     }
+#     # For health checks to read the mount table.
+#     path "sys/mounts" {
+#       capabilities = ["read"]
+#     }
+# EOT
+# }
 
-# Vault AppRole
-# -------------
-resource "vault_auth_backend" "approle" {
-  type = "approle"
-}
+# # Vault AppRole
+# # -------------
+# resource "vault_auth_backend" "approle" {
+#   type = "approle"
+# }
 
-resource "vault_approle_auth_backend_role" "approle" {
-  backend        = vault_auth_backend.approle.path
-  role_name      = var.vault_role_name
-  token_policies = [vault_policy.ptfe.name]
-}
+# resource "vault_approle_auth_backend_role" "approle" {
+#   backend        = vault_auth_backend.approle.path
+#   role_name      = var.vault_role_name
+#   token_policies = [vault_policy.ptfe.name]
+# }
 
-resource "vault_approle_auth_backend_role_secret_id" "approle" {
-  backend   = vault_auth_backend.approle.path
-  role_name = vault_approle_auth_backend_role.approle.role_name
-}
+# resource "vault_approle_auth_backend_role_secret_id" "approle" {
+#   backend   = vault_auth_backend.approle.path
+#   role_name = vault_approle_auth_backend_role.approle.role_name
+# }
 
-resource "vault_mount" "transit" {
-  path = "transit"
-  type = "transit"
-}
+# resource "vault_mount" "transit" {
+#   path = "transit"
+#   type = "transit"
+# }
--- fixtures/test_hcp_vault/outputs.tf
+++ fixtures/test_hcp_vault/outputs.tf
@@ -1,14 +1,14 @@
-output "url" {
-  value       = hcp_vault_cluster.test.vault_public_endpoint_url
-  description = "The public endpoint of the HCP Vault Cluster."
-}
+# output "url" {
+#   value       = hcp_vault_cluster.test.vault_public_endpoint_url
+#   description = "The public endpoint of the HCP Vault Cluster."
+# }
 
-output "app_role_id" {
-  value       = vault_approle_auth_backend_role.approle.role_id
-  description = "The role ID of the Vault's app role."
-}
+# output "app_role_id" {
+#   value       = vault_approle_auth_backend_role.approle.role_id
+#   description = "The role ID of the Vault's app role."
+# }
 
-output "app_role_secret_id" {
-  value       = vault_approle_auth_backend_role_secret_id.approle.secret_id
-  description = "The secret ID of the Vault's app role."
-}
+# output "app_role_secret_id" {
+#   value       = vault_approle_auth_backend_role_secret_id.approle.secret_id
+#   description = "The secret ID of the Vault's app role."
+# }
