7901dfe3b9576323e07c778f87e5bbc9bd3cde63
anniehedgpeth <annie@hashicorp.com>
2023-04-08 11:53:44 -0600

    fmt

--- fixtures/test_bastion_init/outputs.tf
+++ fixtures/test_bastion_init/outputs.tf
@@ -1,4 +1,4 @@
 output "custom_data_setup_ssh" {
-    value       = base64encode(local.setup_ssh)
-    description = "A Base64 encoded, rendered template of the cloud-init script that will allow for SSH to the TFE instance."
+  value       = base64encode(local.setup_ssh)
+  description = "A Base64 encoded, rendered template of the cloud-init script that will allow for SSH to the TFE instance."
 }
