1ead2d0f23fe526d7de100477736eb1e9c70dc8f
anniehedgpeth <annie@hashicorp.com>
2023-09-05 14:06:24 -0600

    null defaults for cloud storage

--- modules/docker_compose_config/variables.tf
+++ modules/docker_compose_config/variables.tf
@@ -89,77 +89,92 @@ variable "storage_type" {
 }
 
 variable "s3_access_key_id" {
+  default     = null
   type        = string
   description = "S3 access key ID. Required when TFE_OBJECT_STORAGE_TYPE is s3 and TFE_OBJECT_STORAGE_S3_USE_INSTANCE_PROFILE is false."
 }
 
 variable "s3_secret_access_key" {
+  default     = null
   type        = string
   description = "S3 secret access key. Required when TFE_OBJECT_STORAGE_TYPE is s3 and TFE_OBJECT_STORAGE_S3_USE_INSTANCE_PROFILE is false."
 
 }
 
 variable "s3_region" {
+  default     = null
   type        = string
   description = "S3 region. Required when TFE_OBJECT_STORAGE_TYPE is s3."
 }
 
 variable "s3_bucket" {
+  default     = null
   type        = string
   description = "S3 bucket name. Required when TFE_OBJECT_STORAGE_TYPE is s3."
 }
 
 variable "s3_endpoint" {
+  default     = null
   type        = string
   description = "S3 endpoint. Useful when using a private S3 endpoint. Leave blank to use the default AWS S3 endpoint. Defaults to \"\" if no value is given."
 }
 
 variable "s3_server_side_encryption" {
+  default     = null
   type        = string
   description = "Server-side encryption algorithm to use. Set to aws:kms to use AWS KMS. Leave blank to disable server-side encryption. Defaults to \"\" if no value is given."
 }
 
 variable "s3_server_side_encryption_kms_key_id" {
+  default     = null
   type        = string
   description = "KMS key ID to use for server-side encryption. Leave blank to use AWS-managed keys. Defaults to \"\" if no value is given."
 }
 
 variable "s3_use_instance_profile" {
+  default     = null
   type        = string
   description = "Whether to use the instance profile for authentication. Defaults to false if no value is given."
 }
 
 variable "azure_account_key" {
+  default     = null
   type        = string
   description = "Azure Blob Storage access key. Required when TFE_OBJECT_STORAGE_TYPE is azure and TFE_OBJECT_STORAGE_AZURE_USE_MSI is false."
 }
 
 variable "azure_account_name" {
+  default     = null
   type        = string
   description = "Azure Blob Storage account name. Required when TFE_OBJECT_STORAGE_TYPE is azure."
 }
 
 variable "azure_container" {
+  default     = null
   type        = string
   description = "Azure Blob Storage container name. Required when TFE_OBJECT_STORAGE_TYPE is azure."
 }
 
 variable "azure_endpoint" {
+  default     = null
   type        = string
   description = "Azure Storage endpoint. Useful if using a private endpoint for Azure Stoage. Leave blank to use the default Azure Storage endpoint. Defaults to \"\" if no value is given. "
 }
 
 variable "google_bucket" {
+  default     = null
   type        = string
   description = "Google Cloud Storage bucket name. Required when TFE_OBJECT_STORAGE_TYPE is google."
 }
 
 variable "google_credentials" {
+  default     = null
   type        = string
   description = "Google Cloud Storage JSON credentials. Must be given as an escaped string of JSON or Base64 encoded JSON. Leave blank to use the attached service account. Defaults to \"\" if no value is given."
 }
 
 variable "google_project" {
+  default     = null
   type        = string
   description = "Google Cloud Storage project name. Required when TFE_OBJECT_STORAGE_TYPE is google."
 }
