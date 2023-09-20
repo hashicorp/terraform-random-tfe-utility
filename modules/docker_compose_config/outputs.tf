0c9a5a993ff369814540b8dc62a96a9f49a09dee
anniehedgpeth <annie@hashicorp.com>
2023-09-20 16:18:58 -0600

    terraform fmt

--- modules/docker_compose_config/outputs.tf
+++ modules/docker_compose_config/outputs.tf
@@ -2,6 +2,6 @@
 # SPDX-License-Identifier: MPL-2.0
 
 output "docker_compose_yaml" {
-  value = yamlencode(local.compose)
+  value       = yamlencode(local.compose)
   description = "A yaml object that will be used as the Docker Compose file for TFE deployment."
 }
