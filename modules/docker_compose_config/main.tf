bbad9c04d41988f31f33227eccc37ce8058ef87b
Annie Hedgpeth <annie@hashicorp.com>
2023-10-26 17:20:00 -0600

    add http prefix to proxy env vars

--- modules/docker_compose_config/main.tf
+++ modules/docker_compose_config/main.tf
@@ -18,10 +18,10 @@ locals {
           local.storage_configuration,
           local.vault_configuration,
           {
-            http_proxy                    = var.http_proxy
-            HTTP_PROXY                    = var.http_proxy
-            https_proxy                   = var.https_proxy
-            HTTPS_PROXY                   = var.https_proxy
+            http_proxy                    = "http://${var.http_proxy}"
+            HTTP_PROXY                    = "http://${var.http_proxy}"
+            https_proxy                   = "http://${var.https_proxy}"
+            HTTPS_PROXY                   = "http://${var.https_proxy}"
             no_proxy                      = join(",", var.no_proxy)
             NO_PROXY                      = join(",", var.no_proxy)
             TFE_HOSTNAME                  = var.hostname
