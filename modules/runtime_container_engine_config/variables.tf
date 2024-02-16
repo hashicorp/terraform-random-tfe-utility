4bb359eabc1c45d96ef9a0223e10efe085d8ddea
John Kerry <john.kerry@hashicorp.com>
2024-02-16 13:16:40 -0500

    default metrics switch to false.

--- modules/runtime_container_engine_config/variables.tf
+++ modules/runtime_container_engine_config/variables.tf
@@ -144,7 +144,7 @@ variable "key_file" {
 }
 
 variable "metrics_endpoint_enabled" {
-  default     = null
+  default     = false
   type        = bool
   description = "(Optional) Metrics are used to understand the behavior of Terraform Enterprise and to troubleshoot and tune performance. Enable an endpoint to expose container metrics. Defaults to false."
 }
