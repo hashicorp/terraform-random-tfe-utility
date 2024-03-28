47de1f23e86d520d3c2855a1426a0e2368a260f1
Theo Skolnik <theo.skolnik@hashicorp.com>
2024-03-28 11:03:46 -0700

    supports special db chars

--- modules/runtime_container_engine_config/database_config.tf
+++ modules/runtime_container_engine_config/database_config.tf
@@ -4,7 +4,7 @@
 locals {
   database = {
     TFE_DATABASE_USER       = var.database_user
-    TFE_DATABASE_PASSWORD   = var.database_password
+    TFE_DATABASE_PASSWORD   = replace(var.database_password, "$", "\\$\\$")
     TFE_DATABASE_HOST       = var.database_host
     TFE_DATABASE_NAME       = var.database_name
     TFE_DATABASE_PARAMETERS = var.database_parameters
