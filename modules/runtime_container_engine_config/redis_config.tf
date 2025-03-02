474e43b07c3ee967ade5161baf365eaf5a3b883f
John Kerry <john.kerry@hashicorp.com>
2025-03-02 13:22:30 -0500

    inverting parameter order for the join function

--- modules/runtime_container_engine_config/redis_config.tf
+++ modules/runtime_container_engine_config/redis_config.tf
@@ -9,7 +9,7 @@ locals {
     TFE_REDIS_USE_TLS              = var.redis_use_tls
     TFE_REDIS_USE_AUTH             = var.redis_use_auth
     TFE_REDIS_SENTINEL_ENABLED     = var.redis_use_sentinel
-    TFE_REDIS_SENTINEL_HOSTS       = join(var.redis_sentinel_hosts, ",")
+    TFE_REDIS_SENTINEL_HOSTS       = join(",", var.redis_sentinel_hosts)
     TFE_REDIS_SENTINEL_LEADER_NAME = var.redis_sentinel_leader_name
     TFE_REDIS_SENTINEL_PASSWORD    = var.redis_sentinel_password
     TFE_REDIS_SENTINEL_USERNAME    = var.redis_sentinel_user
