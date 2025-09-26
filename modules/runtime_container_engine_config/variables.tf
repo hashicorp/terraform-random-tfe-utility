e3e43dd8d8bf74fcfe2c54f7bcfee6207471959c
RAVI PRAKASH <ravi.prakash@hashicorp.com>
2025-09-26 14:10:27 +0530

    Add AWS IAM database authentication support
    - Add database_passwordless_aws_use_iam and database_passwordless_aws_region variables
    - Configure DATABASE_AUTH_USE_AWS_IAM and DATABASE_AUTH_AWS_DB_REGION environment variables
    - Enable AWS IAM passwordless authentication for TFE runtime containers

--- modules/runtime_container_engine_config/variables.tf
+++ modules/runtime_container_engine_config/variables.tf
@@ -118,6 +118,18 @@ variable "database_passwordless_aws_region" {
   description = "AWS region for IAM database authentication. Required when database_passwordless_aws_use_iam is true."
 }
 
+variable "database_passwordless_aws_use_iam" {
+  default     = false
+  type        = bool
+  description = "Whether or not to use AWS IAM authentication to connect to the PostgreSQL database. Defaults to false if no value is given."
+}
+
+variable "database_passwordless_aws_region" {
+  default     = ""
+  type        = string
+  description = "AWS region for IAM database authentication. Required when database_passwordless_aws_use_iam is true."
+}
+
 variable "explorer_database_host" {
   type        = string
   default     = null
